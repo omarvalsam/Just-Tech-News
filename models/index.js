e4183e198d9979fe688389af11462da892ba5fa7
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-01-28 16:23:00 -0700

    added files that have not been tracked '

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
 // create associations
 User.hasMany(Post, {
@@ -10,4 +11,36 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+// ================================================================
+// votes have been declared to many this way!
+
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+// ================================================================
+// vote foreign keys
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+
+// ================================================================
+module.exports = { User, Post, Vote };
